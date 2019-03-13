3b402d7eeef0300379fc564f040af4a1a4cc74f9
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-03-12 20:59:39 -0700

    Fix #15

---
+++ src/moditect/module-info.java
@@ -0,0 +1,17 @@
+// Generated 12-Mar-2019 using Moditect maven plugin
+module org.codehaus.stax2 {
+    requires transitive java.xml;
+
+    exports org.codehaus.stax2;
+    exports org.codehaus.stax2.evt;
+    exports org.codehaus.stax2.io;
+    exports org.codehaus.stax2.osgi;
+    exports org.codehaus.stax2.ri;
+    exports org.codehaus.stax2.ri.dom;
+    exports org.codehaus.stax2.ri.evt;
+    exports org.codehaus.stax2.ri.typed;
+    exports org.codehaus.stax2.typed;
+    exports org.codehaus.stax2.util;
+    exports org.codehaus.stax2.validation;
+
+}
